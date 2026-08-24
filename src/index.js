55e43ea01212df5c9f0a8c043a6c257e230a6a83
Aquib Ahmad rja <aquibahmadrja@gmail.com>
2026-08-24 18:32:53 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.on("error", (err) => {
+      console.error("Error connecting to MongoDB:", err);
+      throw err;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with an error code
+});
 
 
 
